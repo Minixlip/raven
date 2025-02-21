8210019cb039793da6a88533d9bc21743b328f59
Mo Shihab <Minixliplol1@gmail.com>
2025-02-21 15:22:33 +0000

    fixed naming

--- app/components/Misc/Card.tsx
+++ app/components/Misc/Card.tsx
@@ -3,18 +3,15 @@ import { motion } from 'framer-motion';
 import { useState } from 'react';
 
 export default function Card(props) {
-  const [primaryPhoto, setPrimaryPhoto] = useState([
-    props.cardInfo.image,
-    props.cardInfo.secondImage,
-  ]);
+  const primaryPhotos = [props.cardInfo.image, props.cardInfo.secondImage];
 
-  const [displayPhoto, setDisplayPhoto] = useState(primaryPhoto[0]);
+  const [displayPhoto, setDisplayPhoto] = useState(primaryPhotos[0]);
 
   return (
     <div
-      className='flex flex-col border-2 border-neutral-500 w-fit cursor-pointer'
-      onMouseEnter={() => setDisplayPhoto(primaryPhoto[1])}
-      onMouseLeave={() => setDisplayPhoto(primaryPhoto[0])}
+      className="flex flex-col border-2 border-neutral-500 w-fit cursor-pointer"
+      onMouseEnter={() => setDisplayPhoto(primaryPhotos[1])}
+      onMouseLeave={() => setDisplayPhoto(primaryPhotos[0])}
     >
       <motion.div
         style={{
@@ -26,16 +23,16 @@ export default function Card(props) {
           height: '100%',
           zIndex: -1,
         }}
-        className='min-h-96 min-w-96 border-b-2 border-neutral-500'
+        className="md:min-h-[500px] md:min-w-[500px] border-b-2 border-neutral-500"
       />
-      <div className='flex flex-col p-4 gap-3'>
-        <span className='text-xl font-bold text-neutral-500'>
+      <div className="flex flex-col p-4 gap-3">
+        <span className="text-xl font-bold text-neutral-500">
           {props.cardInfo.TypeOfClothing}
         </span>
-        <span className='text-xl font-bold'>
+        <span className="text-xl font-bold">
           {props.cardInfo.NameOfClothing}
         </span>
-        <span className='text-xl font-bold'>${props.cardInfo.Price}</span>
+        <span className="text-xl font-bold">${props.cardInfo.Price}</span>
       </div>
     </div>
   );
