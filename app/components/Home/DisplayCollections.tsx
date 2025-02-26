8601f18fa25a7135d125caf697ad4c579ead3add
Mo Shihab <Minixliplol1@gmail.com>
2025-02-26 20:50:57 +0000

    finished display collections page, and added instagram display

---
+++ app/components/Home/DisplayCollections.tsx
@@ -0,0 +1,81 @@
+'use client';
+
+import { BsArrowUpRight } from 'react-icons/bs';
+import { useState } from 'react';
+import { motion } from 'framer-motion';
+
+export default function DisplayCollections() {
+  const [leftPreviewHover, setLeftPreviewHover] = useState(false);
+  const [rightPreviewHover, setRightPreviewHover] = useState(false);
+
+  return (
+    <div className="min-h-[90vh] flex flex-col md:flex-row md:gap-2 py-2">
+      <div
+        className="flex-1 border border-neutral-500 flex flex-col justify-end font-bold cursor-pointer"
+        style={{
+          backgroundImage: `url(https://framerusercontent.com/images/Bqu1YbtLNP6KpNMpw9Wnp1oQOJA.jpg?scale-down-to=1024)`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'top',
+        }}
+        onMouseEnter={() => setLeftPreviewHover(true)}
+        onMouseLeave={() => setLeftPreviewHover(false)}
+        onClick={() => (location.href = `/collections`)}
+      >
+        <div className="mx-2 my-2 text-3xl flex gap-2 items-center font-semibold">
+          <motion.span
+            animate={{
+              textDecoration: leftPreviewHover ? 'underline' : 'none',
+            }}
+            transition={{ duration: 0.3 }}
+          >
+            SHOP NOW
+          </motion.span>
+          <motion.div
+            animate={{ y: leftPreviewHover ? -5 : 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <BsArrowUpRight />
+          </motion.div>
+        </div>
+        <span className="mx-2 mb-10 text-3xl md:text-7xl tracking-wider">
+          EXPLORE
+          <br /> OUR <br /> COLLECTIONS
+        </span>
+      </div>
+      <div
+        className="flex-1 border border-neutral-500 flex flex-col justify-end font-bold cursor-pointer"
+        style={{
+          backgroundImage: `url(https://framerusercontent.com/images/aHmupIkpNbiTWcrio0jHVxTg4OU.png?scale-down-to=1024)`,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          backgroundPosition: 'top',
+        }}
+        onMouseEnter={() => setRightPreviewHover(true)}
+        onMouseLeave={() => setRightPreviewHover(false)}
+        onClick={() => (location.href = `/about`)}
+      >
+        <div className="mx-2 my-2 text-3xl flex gap-2 items-center font-semibold">
+          <motion.span
+            animate={{
+              textDecoration: rightPreviewHover ? 'underline' : 'none',
+            }}
+            transition={{ duration: 0.3 }}
+          >
+            ABOUT US
+          </motion.span>
+          <motion.div
+            animate={{ y: rightPreviewHover ? -5 : 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <BsArrowUpRight />
+          </motion.div>
+        </div>
+        <span className="mx-2 mb-10 text-3xl md:text-7xl tracking-wider">
+          BUILT
+          <br /> FOR REAL <br /> PERFORMANCE
+        </span>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,10 @@ import HomeCardSection from './components/Home/HomeCardSection';
 import HomeCardSection2 from './components/Home/HomeCardSection2';
 import DisplayClothes from './components/Home/DisplayClothesComp/DisplayClothes';
 import HomeNavigationCards from './components/Home/HomeNavigationCards';
+import DisplayCollections from './components/Home/DisplayCollections';
+
+import { FaInstagramSquare } from 'react-icons/fa';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -14,7 +18,20 @@ export default function Home() {
       <HomeCardSection2 />
       <DisplayClothes />
       <HomeNavigationCards />
-      <div className="min-h-screen">hhh</div>
+      <DisplayCollections />
+      <div className="min-h-[30vh] flex flex-col px-8 text-wrap justify-center">
+        <span className="font-bold tracking-tight text-5xl">FOLLOW RAVEN</span>
+        <span className="font-semibold tracking-tight text-neutral-500 text-3xl">
+          WEAR IT YOUR WAY. TAG US ON INSTAGRAM FOR YOUR CHANCE TO BE FEATURED.
+        </span>
+        <Link href={'https://www.instagram.com'}>
+          <FaInstagramSquare
+            size={40}
+            color="grey"
+            className="mt-4"
+          />
+        </Link>
+      </div>
     </main>
   );
 }
